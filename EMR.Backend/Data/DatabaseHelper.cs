3b2106751bf89ca54ad87041988562016b11cc0c
SilverStryker <rykerellingworth@gmail.com>
2026-05-12 23:04:22 -0500

    Changed password in databasehelper for repo

--- EMR.Backend/Data/DatabaseHelper.cs
+++ EMR.Backend/Data/DatabaseHelper.cs
@@ -5,7 +5,7 @@ namespace EMR.Backend.Data
 {
     public static class DatabaseHelper
     {
-        private static string _connStr = "Server=localhost;Database=EMRKS;Uid=root;Pwd=eLLi2394!;";
+        private static string _connStr = "Server=localhost;Database=EMRKS;Uid=root;Pwd=YourPassword;";
 
         public static MySqlConnection GetConnection()
         {
